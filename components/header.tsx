76f9af5d23d9d6dd6d760db5f177a8a064f18b54
Aman Kumar <amankumarr2005ak@gmail.com>
2025-11-26 13:55:38 +0530

    feat: implement navigation items and update logo format; add constants for investment goals and risk tolerance options

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image"
 import Link from "next/link"
-
+import NavItems from "./ui/navItems"
 const Header = () => {
   return (
    <header className="fixed top-0 left-0 right-0 z-50 w-full">
     <div className="container header-wrapper">
         <Link href= "/">
-        <Image src="/assets/icons/logo.png" alt="Tradexlogo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
+        <Image src="/assets/icons/logo.svg" alt="Tradexlogo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
         </Link>
     </div>
+
+    <nav className="hidden sm:block">
+    </nav>
    </header>
   )
 }
