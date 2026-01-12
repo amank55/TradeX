2b7595cd7a53e73edfdbfafb9b867c7d6973b84b
Aman Kumar <amankumarr2005ak@gmail.com>
2026-01-12 12:57:21 +0530

    feat: enhance SignUp component with form handling and styling; add green button styles

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,45 @@
+"use client"
+
+import { Button } from "@/components/button";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState:{errors, isSubmitting},
+  } = useForm<SignUpFormData>({
+        defaultValues: {
+         fullName: "",
+         email: "",
+         password: "",
+        country: "India",
+        riskTolerance: "medium",
+        preferredIndustry:"technology",
+        },
+        mode: "onBlur"
+  });
+
+  const onSubmit: SubmitHandler<SignUpFormData> = async (data: SignUpFormData) => {
+    try {
+      console.log(data)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+   
+
   return (
-   <>
-   <h1 className="form-title ">Signup& personalize</h1>
-   </>
-  )
-}
-export default SignUp
+    <>
+      <h1 className="form-title">Sign Up & Personalize</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+       <Button type="submit" disabled={isSubmitting} className="green-btn w-full mt-5">
+        {isSubmitting? 'create Account' : 'Start Your Free Journey'}
+       </Button>
+      </form>
+    </>
+  );
+};
+
+export default SignUp;
