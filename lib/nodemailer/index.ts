e758e34b553a11b29302c29d6a2c4a44479955ed
Aman Kumar <amankumarr2005ak@gmail.com>
2026-01-15 17:21:10 +0530

    feat: implement sendWelcomeEmail function and add inngest-cli dependency

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer"
+import { WELCOME_EMAIL_TEMPLATE } from "./templates";
 export const transporter = nodemailer.createTransport({
     service : "gmail",
     auth:{
@@ -6,3 +7,18 @@ export const transporter = nodemailer.createTransport({
         pass: process.env.NODEMAILER_PASSWORD
     }
 })
+export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData) => {
+    const htmlTemplate = WELCOME_EMAIL_TEMPLATE
+        .replace('{{name}}', name)
+        .replace('{{intro}}', intro);
+
+    const mailOptions = {
+        from: `"Signalist" <signalist@jsmastery.pro>`,
+        to: email,
+        subject: `Welcome to Signalist - your stock market toolkit is ready!`,
+        text: 'Thanks for joining Signalist',
+        html: htmlTemplate,
+    }
+
+    await transporter.sendMail(mailOptions);
+}
