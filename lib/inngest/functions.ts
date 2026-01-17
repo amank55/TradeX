482766164edc1c9f7aaf07a63729e5a2c49693e2
Aman Kumar <amankumarr2005ak@gmail.com>
2026-01-17 12:33:20 +0530

    fix: update cron schedule format for daily news summary function

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -50,7 +50,7 @@ export const sendSignUpEmail = inngest.createFunction(
 
 export const sendDailyNewsSummary = inngest.createFunction(
     {id : 'daily-news-summary'},
-    [{event: 'app/send.daily.news'}, {cron: '0 9 * * *'}],
+    [ {cron: 'TZ=Asia/Kolkata 0 9 * * *'}],
     async ({step}) => {
         const users = await step.run('get-all-users', getAllUsersForNewsEmail) as UserForNewsEmail[];
         
